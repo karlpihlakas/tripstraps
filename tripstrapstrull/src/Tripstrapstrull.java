8c04359bb727c07a7fd9fe71f24bcc95d679db29
Tauri Miilits <taurimiilits@Tauris-MacBook-Pro.local>
2021-04-15 19:47:13 +0300

    Game updated. Added Win/Lose/Draw

--- tripstrapstrull/src/Tripstrapstrull.java
+++ tripstrapstrull/src/Tripstrapstrull.java
@@ -4,11 +4,17 @@
 //
 
 package src;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 //import java.util.Arrays;
 import java.util.Scanner;
 
 public class Tripstrapstrull {
+
+    static ArrayList<Integer> playerPos = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPos = new ArrayList<Integer>();
 
     public static void main(String[] args) {
 
@@ -21,17 +27,28 @@ public class Tripstrapstrull {
         printTable(table);
 
         while(true) {
-            Scanner scan = new Scanner(System.in);
             System.out.println("Valige koht 1-9:");
+            Scanner scan = new Scanner(System.in);
+            //System.out.println("Valige koht 1-9:");
             int pickedNumber = scan.nextInt();
+            while(playerPos.contains(pickedNumber) || cpuPos.contains(pickedNumber)) { //kui see koht on võetud, siis kasutaja valib uue koha
+                System.out.println("Koht võetud! Vali uus koht");
+                pickedNumber = scan.nextInt();
+            }
 
             placeSymbol(table, pickedNumber, "player");
 
             Random random = new Random();
             int cpuNumber = random.nextInt(9) + 1; //slp et ta võtaks 1-9
+            while(playerPos.contains(cpuNumber) || cpuPos.contains(cpuNumber)) { //arvuti valib seni, kuni leiab uue koha
+                cpuNumber = random.nextInt(9) + 1;
+            }
             placeSymbol(table, cpuNumber, "CPU");
 
             printTable(table);
+
+            String gameResult = checkStatus();
+            System.out.println(gameResult);
         }
     }
     public static void printTable(char [][] table){
@@ -43,18 +60,19 @@ public class Tripstrapstrull {
         }
     }
 
-    public static void placeSymbol(char[][] table, int pickedNumber, String user) {
+    public static void placeSymbol(char[][] table, int pos, String user) {
         
         char symbol = ' ';
-
         if(user.equals("player")) {
             symbol = 'X';
+            playerPos.add(pos);
         } else if(user.equals("CPU")) {
             symbol = 'O';
+            cpuPos.add(pos);
         }
 
-        
-        switch(pickedNumber) {
+        switch(pos) {
+            // 2 dimensional asetus boardil
             case 1:
                 table[0][0] = symbol;
                 break;
@@ -62,7 +80,7 @@ public class Tripstrapstrull {
                 table[0][2] = symbol;
                 break;
             case 3:
-                table[0][3] = symbol;
+                table[0][4] = symbol;
                 break;
             case 4:
                 table[2][0] = symbol;
@@ -83,5 +101,39 @@ public class Tripstrapstrull {
                 table[4][4] = symbol;
                 break;
             }
+    }
+
+    public static String checkStatus() { //siin jätab listi meelde kasutaja ja arvuti käigud ja checkib kas keegi on "võitja" või mitte
+        //nende asetustega saab võita
+        List topRow = Arrays.asList(1, 2, 3);
+        List middleRow = Arrays.asList(4, 5, 6);
+        List bottomRow = Arrays.asList(7, 8, 9);
+        List leftColumn = Arrays.asList(1, 4, 7);
+        List middleColumn = Arrays.asList(2, 5, 8);
+        List rightColumn = Arrays.asList(3, 6, 9);
+        List leftCross = Arrays.asList(1, 5, 9);
+        List rightCross = Arrays.asList(3, 5, 7);
+
+        List<List> win = new ArrayList<List>();
+        win.add(topRow);
+        win.add(middleRow);
+        win.add(bottomRow);
+        win.add(leftColumn);
+        win.add(middleColumn);
+        win.add(rightColumn);
+        win.add(leftCross);
+        win.add(rightCross);
+
+        for (List l : win) {
+            if(playerPos.containsAll(l)) { //kui mängijal on tabelis (listis) mingi ülal toodud asetuse võimalus, siis võidab
+                return "Palju õnne, sa võitsid!";
+            } else if(cpuPos.containsAll(l)) {
+                return "Kahjuks kaotasid";
+            } else if(playerPos.size() + cpuPos.size() == 9) { //kui väljal pole enam ruumi sest nende listide summa annab 9 -> viik
+                return "Mäng jäi viiki. Proovi uuesti!";
+            }
+        }
+
+        return "";
     }
 }
